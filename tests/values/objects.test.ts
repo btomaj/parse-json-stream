3834afafbe5123dc7b07935eb2ecc297a4d97e6f
poetry by <2638986+btomaj@users.noreply.github.com>
2025-08-11 19:49:41 +1000

    Integration test WIP

--- tests/values/objects.test.ts
+++ tests/values/objects.test.ts
@@ -1,58 +1,135 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
+import { parseStream } from "~/index";
 
-describe("Object Parsing", () => {
-  const flatObject =
-    '{"false":false,"true":true,"null":null,"zero":0,"integer":42,"negative_integer":-17,"float":3.14159,"negative_float":-0.001,"scientific_positive":1.23e10,"scientific_negative":-4.56E-7,"scientific_plus":1e+5,"string":"Hello World","empty_string":"","escaped_quote": "\\"","escaped_backslash": "\\\\", "escaped_forwardslash": "\\/", "escaped_backspace": "\\b", "escaped_formfeed": "\\f", "escaped_newline": "\\n", "escaped_carriagereturn": "\\r", "escaped_tab": "\\t", "escaped_unicode": "\\u0041","empty_object":{},"empty_array":[]}';
-  const deepObject =
-    '{"primitives":{"false":false,"true":true,"null":null},"numbers":{"zero":0,"integer":42,"negative_integer":-17,"float":3.14159,"negative_float":-0.001,"scientific_positive":1.23e10,"scientific_negative":-4.56E-7,"scientific_plus":1e+5},"strings":{"string":"Hello World","empty_string":"","escaped_quote": "\\"","escaped_backslash": "\\\\", "escaped_forwardslash": "\\/", "escaped_backspace": "\\b", "escaped_formfeed": "\\f", "escaped_newline": "\\n", "escaped_carriagereturn": "\\r", "escaped_tab": "\\t", "escaped_unicode": "\\u0041"},"containers":{"empty_object":{},"empty_array":[],"level2":[{"level4": {"null": null, "false": false, "true": true, "meaning_of_life": 42, "string": "deep thought"}}]}}';
-  const malformedObjects =
-    '{"key"}{"key":}{"key",}{"key":,}{"key",:}{:}{,}{:,}{,:}{,"key"}{:"value"}';
+const flatObject =
+  '{"false":false,"true":true,"null":null,"zero":0,"integer":42,"negative_integer":-17,"float":3.14159,"negative_float":-0.001,"scientific_positive":1.23e10,"scientific_negative":-4.56E-7,"scientific_plus":1e+5,"string":"Hello World","empty_string":"","escaped_quote": "\\"","escaped_backslash": "\\\\", "escaped_forwardslash": "\\/", "escaped_backspace": "\\b", "escaped_formfeed": "\\f", "escaped_newline": "\\n", "escaped_carriagereturn": "\\r", "escaped_tab": "\\t", "escaped_unicode": "\\u0041","empty_object":{},"empty_array":[]}';
+const deepObject =
+  '{"primitives":{"false":false,"true":true,"null":null},"numbers":{"zero":0,"integer":42,"negative_integer":-17,"float":3.14159,"negative_float":-0.001,"scientific_positive":1.23e10,"scientific_negative":-4.56E-7,"scientific_plus":1e+5},"strings":{"string":"Hello World","empty_string":"","escaped_quote": "\\"","escaped_backslash": "\\\\", "escaped_forwardslash": "\\/", "escaped_backspace": "\\b", "escaped_formfeed": "\\f", "escaped_newline": "\\n", "escaped_carriagereturn": "\\r", "escaped_tab": "\\t", "escaped_unicode": "\\u0041"},"containers":{"empty_object":{},"empty_array":[],"level2":[{"level4": {"null": null, "false": false, "true": true, "meaning_of_life": 42, "string": "deep thought"}}]}}';
 
-  describe("should parse flat objects", () => {
-    const variants = [];
-    for (let chunkSize = 0; chunkSize < flatObject.length + 1; chunkSize++) {
-      // split into two chunks based on chunk size
-      variants.push({
-        chunks: [flatObject.slice(0, chunkSize), flatObject.slice(chunkSize)],
-      });
-      if (chunkSize === 0) {
-        continue;
-      }
-      // split into n chunks based on chunk size
-      const numberOfChunks = Math.ceil(flatObject.length / chunkSize);
-      if (numberOfChunks === 1 || numberOfChunks === 2) {
-        continue;
-      }
-      const chunks = [];
-      for (let i = 0; i < numberOfChunks; i += 1) {
-        chunks.push(flatObject.slice(i * chunkSize, (i + 1) * chunkSize));
-      }
-      variants.push({ chunks });
+class StructureBuilder {
+  private root: any = null;
+
+  set(path: Array<string | number>, value: string): void {
+    if (this.root === null) {
+      this.root = typeof path[0] === "number" ? [] : {};
     }
-    it.for(variants)("split into $chunks", () => {});
-  });
 
-  describe("should parse deep objects", () => {
-    const variants = [];
-    for (let chunkSize = 0; chunkSize < deepObject.length + 1; chunkSize++) {
-      // split into two chunks based on chunk size
-      variants.push({
-        chunks: [deepObject.slice(0, chunkSize), deepObject.slice(chunkSize)],
-      });
-      if (chunkSize === 0) {
-        continue;
+    let curr: any = this.root;
+
+    for (let i = 0; i < path.length; i++) {
+      const key = path[i];
+      const isLast = i === path.length - 1;
+
+      if (isLast) {
+        if (curr[key] === undefined) {
+          curr[key] = "";
+        }
+        curr[key] += value;
+      } else {
+        const nextKey = path[i + 1];
+        const shouldBeArray = typeof nextKey === "number";
+
+        if (!(key in curr)) {
+          curr[key] = shouldBeArray ? [] : {};
+        }
+
+        curr = curr[key];
       }
-      // split into n chunks based on chunk size
-      const numberOfChunks = Math.ceil(deepObject.length / chunkSize);
-      if (numberOfChunks === 1 || numberOfChunks === 2) {
-        continue;
-      }
-      const chunks = [];
-      for (let i = 0; i < numberOfChunks; i += 1) {
-        chunks.push(deepObject.slice(i * chunkSize, (i + 1) * chunkSize));
-      }
-      variants.push({ chunks });
     }
-    it.for(variants)("split into $chunks", () => {});
+  }
+
+  getStructure(): any {
+    return this.root;
+  }
+}
+
+describe("flat objects", () => {
+  const variants = [];
+  for (let chunkSize = 0; chunkSize < flatObject.length + 1; chunkSize++) {
+    // split into two chunks based on chunk size
+    variants.push([
+      flatObject.slice(0, chunkSize),
+      flatObject.slice(chunkSize),
+    ]);
+    if (chunkSize === 0) {
+      continue;
+    }
+    // split into n chunks based on chunk size
+    const numberOfChunks = Math.ceil(flatObject.length / chunkSize);
+    if (numberOfChunks === 1 || numberOfChunks === 2) {
+      continue;
+    }
+    const chunks = [];
+    for (let i = 0; i < numberOfChunks; i += 1) {
+      chunks.push(flatObject.slice(i * chunkSize, (i + 1) * chunkSize));
+    }
+    variants.push(chunks);
+  }
+  describe.for(variants)("split into %o", (variant) => {
+    it("should ", async () => {
+      const readableStream = new ReadableStream({
+        start(controller) {
+          for (const chunk of variant) {
+            controller.enqueue(chunk);
+          }
+          controller.close();
+        },
+      });
+      const builder = new StructureBuilder();
+      const jsonChunks = parseStream(readableStream);
+
+      for await (const chunk of jsonChunks) {
+        builder.set(chunk.segments, chunk.value);
+      }
+
+      expect(builder.getStructure()).toEqual({
+        escaped_backslash: "\\\\",
+        escaped_backspace: "\\b",
+        escaped_carriagereturn: "\\r",
+        escaped_formfeed: "\\f",
+        escaped_forwardslash: "\\/",
+        escaped_newline: "\\n",
+        escaped_quote: '\\"',
+        escaped_tab: "\\t",
+        escaped_unicode: "\\u0041",
+        false: "false",
+        float: "3.14159",
+        integer: "42",
+        negative_float: "-0.001",
+        negative_integer: "-17",
+        null: "null",
+        scientific_negative: "-4.56E-7",
+        scientific_plus: "1e+5",
+        scientific_positive: "1.23e10",
+        string: "Hello World",
+        true: "true",
+        zero: "0",
+      });
+    });
   });
+});
+
+describe("nested objects", () => {
+  const variants = [];
+  for (let chunkSize = 0; chunkSize < deepObject.length + 1; chunkSize++) {
+    // split into two chunks based on chunk size
+    variants.push([
+      deepObject.slice(0, chunkSize),
+      deepObject.slice(chunkSize),
+    ]);
+    if (chunkSize === 0) {
+      continue;
+    }
+    // split into n chunks based on chunk size
+    const numberOfChunks = Math.ceil(deepObject.length / chunkSize);
+    if (numberOfChunks === 1 || numberOfChunks === 2) {
+      continue;
+    }
+    const chunks = [];
+    for (let i = 0; i < numberOfChunks; i += 1) {
+      chunks.push(deepObject.slice(i * chunkSize, (i + 1) * chunkSize));
+    }
+    variants.push(chunks);
+  }
+  it.for(variants)("split into chunks", () => {});
 });
